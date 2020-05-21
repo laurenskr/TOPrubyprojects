2c4e26d9e78bc433f80dc2466fa960b84e3f4d92
Laurens Krom <laurens.krom@gmail.com>
2020-05-21 23:25:45 +0200

    created bubbel sort

---
+++ bubble_sort.rb
@@ -0,0 +1,13 @@
+def bubble_sort(array)
+    array.length.times do
+      for i in 0..array.length-2
+        if array[i] >= array[i+1]
+          array[i], array[i+1] = array[i+1], array[i]
+        end
+      end
+    end
+  #returns a sorted array 
+    return array
+  end
+  
+  bubble_sort([4,3,78,2,0,2])
